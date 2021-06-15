5c1ab83571d962cf36d616ad9836300d662d32f5
gopal das <gopal1409@gmail.com>
2021-06-15 02:31:30 -0700

    pythondockerapp

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
-from logging import debug
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "welcome to flask app"
-if __name__== "__main__"
-        app.run(host='0.0.0.0', port = 5001, debug=True)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
--- dockerfile
+++ dockerfile
@@ -1,6 +1,7 @@
 FROM python:rc-alpine3.13
 COPY . /app
-RUN pip install flask
+WORKDIR /app
+RUN pip install -r requirements.txt
 EXPOSE 5001
 ENTRYPOINT ["python"]
-CMD ["demo.py]
+CMD ["demo.py"]
